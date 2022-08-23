7ab1852d1e3ebc6d785063aa1ea0251aa58c2b54
Datngo2001 <ngominhdat110115@gmail.com>
2022-08-23 20:28:25 +0700

    go to article, and go to profile

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -10,31 +10,54 @@ import {
 import React from 'react';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { convertDate } from '../util/convertDate';
+import { useNavigate } from 'react-router';
 
 function ArticleCard({ article }) {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/article/${article._id}`);
+  };
+
+  const gotoAuthorProfile = () => {
+    navigate(`/profile/${article.author._id}`);
+  };
+
   return (
     <Card variant="outlined">
+      <div
+        style={{ cursor: 'pointer', borderBottom: '1px solid #f2f2f2' }}
+        onClick={handleCardClick}>
+        <CardMedia
+          component="img"
+          height="300"
+          image={article.thumbnail}
+          alt={article.title}></CardMedia>
+        <CardContent>
+          <Typography variant="h5">{article.title}</Typography>
+          <Typography variant="body1">{article.subtitle}</Typography>
+        </CardContent>
+      </div>
       <CardHeader
         avatar={
-          <Avatar src={article.author.avatar} alt={article.author.username || 'avartar'}></Avatar>
+          <Avatar
+            style={{ cursor: 'pointer' }}
+            onClick={() => gotoAuthorProfile()}
+            src={article.author.avatar}
+            alt={article.author.username || 'avartar'}></Avatar>
         }
         action={
           <IconButton aria-label="settings">
             <MoreVertIcon />
           </IconButton>
         }
-        title={article.author.username || article.author.email}
+        title={
+          <Typography style={{ cursor: 'pointer' }} onClick={() => gotoAuthorProfile()}>
+            {article.author.username || article.author.email}
+          </Typography>
+        }
         subheader={convertDate(article.createDate)}
       />
-      <CardMedia
-        component="img"
-        height="250"
-        image={article.thumbnail}
-        alt={article.title}></CardMedia>
-      <CardContent>
-        <Typography variant="h5">{article.title}</Typography>
-        <Typography variant="body1">{article.subtitle}</Typography>
-      </CardContent>
     </Card>
   );
 }
